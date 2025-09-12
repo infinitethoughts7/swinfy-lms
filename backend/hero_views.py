a3c87a63fbf224936f21e0b9af2cde0e6cf21a1f
infinitethoughts7 <rakeshganji007@gmail.com>
2025-09-12 10:41:18 +0530

    deleted py script files

--- backend/hero_views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def hero_demo(request):
-    """Render the hero demo template"""
-    return render(request, 'hero_demo.html')
