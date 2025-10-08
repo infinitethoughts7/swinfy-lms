a727c310c864f31bb656d10e299670862110551e
infinitethoughts7 <rakeshganji007@gmail.com>
2025-10-08 18:51:59 +0530

    jwt tokens time setup

--- backend/lms_backend/settings.py
+++ backend/lms_backend/settings.py
@@ -263,8 +263,8 @@ os.makedirs(os.path.join(BASE_DIR, 'logs'), exist_ok=True)
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
